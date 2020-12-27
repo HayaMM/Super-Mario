ce1851d9bc74edab0f485f65e2528ef14e633348
Haya <hm.almadhi@gmail.com>
2020-12-27 10:33:38 +0300

    Added js Jump function

---
+++ script.js
@@ -0,0 +1,7 @@
+var mario = document.getElementsByClassName("superMario");
+var obstacle = document.getElementsByClassName("obstacle");
+function marioJump() {
+    // access the charcter + grap itms of class list add the animation to the function
+    mario.classList.add("animation");
+}
+
