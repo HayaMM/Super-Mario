4c177b8c93d8982389d16e0537199cbb48772d2c
Haya <hm.almadhi@gmail.com>
2020-12-27 14:16:36 +0300

    added collision check function

--- script.js
+++ script.js
@@ -8,12 +8,26 @@ $(document).ready(function () {
     // access the charcter + grap itms of class list add the animation to the function
 
     $('.gameSpace').click(function () {
-        $('.superMario').addClass("animation");
+        //add the class if it's nas not been added
+        if (mario.classList != "animation") {
+            $('.superMario').addClass("animation");
+        }
         //remove class when animation is done
         setTimeout(function () {
             $('.superMario').removeClass("animation");
         }, 500);
     });
+    var clash = setInterval(function () {
+        // get mario's position and obstcale
+        // parseInt used to return only the number without px
+        var marioPo = parseInt(window.getComputedStyle(mario).getPropertyValue("top"));
+        var obstaclePo = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+        if (obstaclePo < 40 && obstaclePo > 0 && marioPo >= 277) {
+            obstaclePo.style.animation = "none";
+            obstaclePo.style.display = "none";
+            alert("You hit obstacle");
+        }
+    }, 10);
 
 
 })
