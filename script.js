0eb3d344bff37a230928422a01b6871d37985ff1
Haya <hm.almadhi@gmail.com>
2020-12-27 11:51:53 +0300

    on click jump function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var mario = document.getElementsByClassName("superMario");
-var obstacle = document.getElementsByClassName("obstacle");
-function marioJump() {
-    // access the charcter + grap itms of class list add the animation to the function
-    mario.classList.add("animation");
-}
+$(document).ready(function () {
 
+    var mario = document.getElementsByClassName("superMario");
+    var obstacle = document.getElementsByClassName("obstacle");
+    //document.getElementsByClassName(".gameSpace").addEventListener("onclick", function () {
+
+    // access the charcter + grap itms of class list add the animation to the function
+    //mario.classList.add(".animation"); });
+
+    $('.gameSpace').click(function () {
+        $('.superMario').addClass("animation");
+    });
+
+    /* $('.gameSpace').addEventListener("click", function () {
+    
+        mario.classList.add("animation");
+    }); */
+})
