15dfa73e780c720519d9932774367fdd080e50e5
Haya <hm.almadhi@gmail.com>
2020-12-28 21:18:37 +0300

    Final ver

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ $(document).ready(function () {
 
     var mario = document.getElementsByClassName("superMario");
     var obstacle = document.getElementsByClassName("obstacle");
-    //document.getElementsByClassName(".gameSpace").addEventListener("onclick", function () {
-    //mario.classList.add(".animation"); });
-
     // access the charcter + grap itms of class list add the animation to the function
-
+    //every click on the game space Mario will jump
     $('.gameSpace').click(function () {
         //add the class if it's nas not been added
         if (mario.classList != "animation") {
@@ -18,25 +15,19 @@ $(document).ready(function () {
         }, 500);
     });
 
-    //$('.test').click(function () {
-
     setInterval(function () {
         // get mario's position and obstcale
         // parseInt used to return only the number without px
+        //get the value of obstacle left position  every 50 ms
         var mmch = $('.superMario')[0];
-        //console.log(mmch);
         var marioPo = parseInt(window.getComputedStyle(mmch).getPropertyValue("top"));
-
-        //console.log(marioPo);
+        //get the value of obstacle left position  every 50 ms
         var mmob = $('.obstacle')[0];
         var obstaclePo = parseInt(window.getComputedStyle(mmob).getPropertyValue("left"));
-        //  console.log(obstaclePo);
-
-        if (obstaclePo < 50 && obstaclePo > 0 && marioPo > 290) {
-            console.log("enter");
-            //obstaclePo.style.animation = "none";
-            //obstaclePo.style.display = "none";
-            alert("You hit obstacle");
+        // check the positions 
+        if (obstaclePo < 50 && obstaclePo > 0 && marioPo > 310) {
+            // Alert lose to the user 
+            alert("You lose  . . .");
             //to reload the page to play again 
             location.reload();
         }
